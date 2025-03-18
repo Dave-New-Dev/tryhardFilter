b17aa77f9d26ef8e3f2c33eb2c5fe6d6f9eecc56
Dave <newdevdave@gmail.com>
2025-03-18 10:38:54 -0400

    hopefully :D

--- src/main/java/dan/tryhardfilter/TryhardChatFilter.java
+++ src/main/java/dan/tryhardfilter/TryhardChatFilter.java
@@ -2,19 +2,17 @@ package dan.tryhardfilter;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.message.v1.ClientSendMessageEvents;
-import net.minecraft.text.Text;
-
 import java.util.regex.Pattern;
 
 public class TryhardChatFilter implements ClientModInitializer {
-    // Case-insensitive regex pattern for "foo"
-    private static final Pattern FOO_PATTERN = Pattern.compile("\\bryan\\b", Pattern.CASE_INSENSITIVE);
+    // Case-insensitive regex pattern for "ryan"
+    private static final Pattern RYAN_PATTERN = Pattern.compile("\\bryan\\b", Pattern.CASE_INSENSITIVE);
 
     @Override
     public void onInitializeClient() {
         // Modify outgoing chat messages before they are sent
         ClientSendMessageEvents.MODIFY_CHAT.register((message) -> {
-            return FOO_PATTERN.matcher(message).replaceAll("tryhard");
+            return RYAN_PATTERN.matcher(message).replaceAll("tryhard");
         });
     }
 }
